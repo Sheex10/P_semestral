883aaa58f122a53b2c21439328d12210b2a8c572
Felipe <feliperivas1709@gmail.com>
2023-08-24 01:16:25 -0400

    ya

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,8 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router, private toastController: ToastController,
+    private alertController: AlertController) { }
+
 
 }
