7e9a2264ff258ebc8112f4afd51ba177a891afbe
Felipe <feliperivas1709@gmail.com>
2023-08-25 16:22:05 -0400

    ya
    aaaa

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 
 
 @NgModule({
@@ -14,7 +14,6 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     FormsModule,
     IonicModule,
     HomePageRoutingModule,
-    MatProgressSpinnerModule
   ],
   declarations: [HomePage]
 })
