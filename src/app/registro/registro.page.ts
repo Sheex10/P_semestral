11fff1e7dddd4dc67c7bf66ab77154167d85e6c3
Felipe <feliperivas1709@gmail.com>
2023-08-31 23:20:43 -0400

    s

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 
@@ -21,7 +22,19 @@ export class RegistroPage implements OnInit {
   FechColor: string = "warning";
 
   flag: string = "false";
-  constructor(public alertController: AlertController, private router:Router) { }
+  formularioR:FormGroup;
+  constructor(public alertController: AlertController, private router:Router, public fb:FormBuilder) { 
+    this.formularioR=this.fb.group({
+      'Correo': new FormControl('',[Validators.required,Validators.minLength(5), Validators.email]),
+      'Contraseña': new FormControl('',[Validators.required,Validators.minLength(5)]),
+      'ConfirmarPwd': new FormControl('',Validators.compose([
+        Validators.maxLength(25),
+        Validators.minLength(5),
+        Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+        Validators.required
+      ]))
+    })
+  }
 
   ngOnInit() {
   }
