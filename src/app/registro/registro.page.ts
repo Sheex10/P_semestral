792072a4ba504e709dc44c203e0d9bbe25f518e4
Felipe <feliperivas1709@gmail.com>
2023-09-13 23:17:22 -0300

    casi nada xd

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import{
+  FormGroup,
+FormControl,
+Validators,
+FormBuilder} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 
@@ -10,18 +14,6 @@ import { AlertController } from '@ionic/angular';
 })
 export class RegistroPage implements OnInit {
 
-  nombre: string = "";
-  fechaNac!: Date;
-  correo: string = "";
-  NomMsj: string = ""; 
-  FechMsj: string = "";
-  CorrMsj: string = "";
-
-  NomColor: string = "warning";
-  CorrColor: string = "warning";
-  FechColor: string = "warning";
-
-  flag: string = "false";
   formularioR:FormGroup;
   constructor(public alertController: AlertController, private router:Router, public fb:FormBuilder) { 
     this.formularioR=this.fb.group({
@@ -48,23 +40,8 @@ export class RegistroPage implements OnInit {
     });
     await alert.present();
   }
-  volver(){
-    this.flag="false";
-  }
 
-  verificarRegistro(name:string,fecha:Date,correo:string){
-    this.NomMsj = ""; this.FechMsj="";
-    this.CorrMsj = ""; this.CorrColor="warning";
-    this.FechColor="warning"; this.NomColor="warning";
 
-    var flag= true;
-
-    /*if (this.numerosName(name)){
-      this.NomMsj = "El nombre no debe contener números";
-      flag = true;
-      this.NomColor="danger";
-    }*/
-  }
   goTologin(){
     this.router.navigate(['/login'])
     
