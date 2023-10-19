199361c2175bf26a936b3594fe31cab810485985
Patreyesa <pat.reyesa@duocuc.cl>
2023-10-19 00:03:47 -0300

    casi cambios
    casi :( changes

--- src/app/editarperfil2/editarperfil2.page.ts
+++ src/app/editarperfil2/editarperfil2.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { AlertController } from '@ionic/angular/providers/alert-controller';
+import { BdserviceService } from '../services/bdservice.service';
 
 
 @Component({
@@ -9,10 +13,157 @@ import { Router } from '@angular/router';
 })
 export class Editarperfil2Page implements OnInit {
 
-  constructor(private router:Router) { }
+  formularioModificar:FormGroup;
+  imagenNueva:any;
 
-  ngOnInit() {
+  arreglousuario:any =[{
+    idU: 0,
+    nombreU: '' ,
+    correo:'',
+    contrasena:'',
+    idPregunta:0,
+    respuesta:'',
+    foto:'',
+    descripcion:''
+  }]
+
+  pedirCorreo="";
+  pedirUsuario="";
+  pedirDesc="";
+
+  idUsuario:any;
+
+  infoUsuario:any;
+
+  prueba=true;
+
+  correoU="";
+  nombre="";
+  descripcion="";
+  fotoN:any;
+  
+  //perfilUsuario=new EventEmitter<any>();
+
+  constructor(public fb:FormBuilder,private router:Router, private activatedRouter:ActivatedRoute, public alertController:AlertController,private bd:BdserviceService) {
+    this.activatedRouter.queryParams.subscribe(param =>{
+      if (this.router.getCurrentNavigation()?.extras.state){
+        this.infoUsuario = this.router.getCurrentNavigation()?.extras?.state?.["infoUsuario"];
+        this.correoU= this.infoUsuario.correo;
+        this.nombre= this.infoUsuario.nombre;
+        this.descripcion= this.infoUsuario.descripcion;
+        this.fotoN=this.infoUsuario.foto;
+        this.idUsuario=this.infoUsuario.idU; 
+
+        this.pedirCorreo=this.correoU;
+        this.pedirUsuario=this.nombre;
+        
+        
+      }
+    })
+
+    this.formularioModificar=this.fb.group({
+      'NombreUsuario': new FormControl("",[Validators.required]),
+      'Descripcion': new FormControl("",[Validators.required]),
+      'Correo': new FormControl("",[Validators.required]),
+    })
+
   }
+  ngOnInit() {
+    /*this.bd.bdstate().subscribe(res => {
+      if (res) {
+        this.bd.fetchUsuario().subscribe(datos => {
+          this.arreglousuario = datos;
+        });
+      }
+    });*/
+  }
+  
+  /*perfil(){
+    this.perfilUsuario.emit(["false"]);
+  }*/
+
+  get correo(){
+    return this.formularioModificar.get('Correo') as FormControl;
+   }
+
+  get nombreU(){
+    return this.formularioModificar.get('NombreUsuario') as FormControl;
+   }
+  
+  get Descri(){
+    return this.formularioModificar.get('Descripcion') as FormControl;
+   }
+
+
+  
+   modificarP() {
+    this.prueba = true;
+
+    if(this.correoU == this.pedirCorreo){
+      this.bd.actualizaPerfilUsuario(this.idUsuario,this.pedirCorreo,this.nombre,this.descripcion,this.fotoN);
+      this.presentAlert("Usuario Modificado");
+      let infoUsuario={
+            idU:this.idUsuario,
+            correo:this.pedirCorreo,
+            nombre:this.nombre,
+            foto:this.fotoN,
+            descripcion:this.descripcion
+        }
+        let navigationextra:NavigationExtras={
+          state:{
+            infoUsuario:infoUsuario }
+        }
+      this.router.navigate(['/perfil-usuario'],navigationextra)
+    }
+
+    if (this.correoU != this.pedirCorreo) {
+      for (let i = 0; i < this.arreglousuario.length; i++) {
+        if (this.pedirCorreo === this.arreglousuario[i].correo) {
+          this.prueba = false;
+          this.presentAlert("Correo ya existente");
+        }
+      }
+      if(this.prueba){
+        this.bd.actualizaPerfilUsuario(this.idUsuario,this.pedirCorreo,this.nombre,this.descripcion ,this.fotoN);
+        this.presentAlert("Usuario Modificado");
+        let infoUsuario={
+          idU:this.idUsuario,
+          correo:this.pedirCorreo,
+          nombre:this.nombre,
+          foto:this.fotoN,
+          descripcion:this.descripcion
+          }
+      let navigationextra:NavigationExtras={
+        state:{
+          infoUsuario:infoUsuario }
+          }
+        this.router.navigate(['/perfil-usuario'],navigationextra)
+      }
+    }
+  }
+
+ 
+
+   async presentAlert( msj:string) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: msj,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  takePicture = async () => {
+    const image2 = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source:CameraSource.Photos
+    });
+    this.fotoN= image2.dataUrl;
+  };
+
   goTomenuadmin(){
     this.router.navigate(['/menuadmin'])
     
